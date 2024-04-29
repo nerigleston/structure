c92891a6c9e753043ea0c893eb217d249f19da32
nerigleston <nerifilho1256@gmail.com>
2024-04-29 13:15:46 -0300

    Refactor folder creation logic in index.js and create initial files for new folder

--- index.js
+++ index.js
@@ -1,32 +1,65 @@
 const readlineSync = require('readline-sync');
-const { mkdirp } = require('mkdirp');
 const fs = require('fs');
+const path = require('path');
 
 const args = process.argv.slice(2);
 const folderType = args[0] || '';
 
 const folderName = readlineSync.question('Digite o nome da pasta: ');
 
-if (folderType !== 'components' && folderType !== 'pages') {
+const validFolderTypes = ['components', 'pages'];
+
+if (!folderName.trim()) {
+    console.error('Nome da pasta não pode estar vazio.');
+    process.exit(1);
+}
+
+if (!validFolderTypes.includes(folderType)) {
     console.error('Tipo de pasta inválido. Use "components" ou "pages".');
     process.exit(1);
 }
 
-mkdirp(`src/${folderType}/${folderName}`).then(() => {
-    const indexPath = `src/${folderType}/${folderName}/index.jsx`;
-    const modelPath = `src/${folderType}/${folderName}/model.jsx`;
-    const viewPath = `src/${folderType}/${folderName}/view.jsx`;
-    const stylePath = `src/${folderType}/${folderName}/style.js`;
+const fileConfig = {
+    components: ['index.jsx', 'style.js'],
+    pages: ['index.jsx', 'model.jsx', 'view.jsx', 'style.js'],
+};
 
-    const indexContent = `import React from 'react';\nimport use${folderName} from './model';\nimport ${folderName}View from './view';\n\nexport default function ${folderName}() {\n  const ${folderName}Model = use${folderName}();\n\n  return <${folderName}View {...${folderName}Model} />;\n}\n`;
-    const modelContent = `export default function use${folderName}() {\n  return {};\n}\n`;
-    const viewContent = `import React from 'react';\nimport * as Styled from './style';\n\nexport default function ${folderName}View() {\n  return (\n    <Styled.ContainerPage></Styled.ContainerPage>\n  );\n}\n`;
-    const styleContent = `import styled from 'styled-components';\n\nexport const ContainerPage = styled.div\`\`;`;
+const folderPath = path.join('src', folderType, folderName);
 
-    fs.writeFileSync(indexPath, indexContent);
-    fs.writeFileSync(modelPath, modelContent);
-    fs.writeFileSync(viewPath, viewContent);
-    fs.writeFileSync(stylePath, styleContent);
+try {
+    fs.mkdirSync(folderPath, { recursive: true });
+
+    const filesToCreate = fileConfig[folderType];
+
+    filesToCreate.forEach((file) => {
+        const filePath = path.join(folderPath, file);
+        let fileContent = '';
+
+        switch (file) {
+            case 'index.jsx':
+                fileContent = folderType === 'components'
+                    ? `import * as Styled from './style';\n\nexport default function ${folderName}() {\n  return (\n    <Styled.ContainerPage></Styled.ContainerPage>\n  );\n}\n`
+                    : `import React from 'react';\nimport use${folderName} from './model';\nimport ${folderName}View from './view';\n\nexport default function ${folderName}() {\n  const ${folderName}Model = use${folderName}();\n\n  return <${folderName}View {...${folderName}Model} />;\n}\n`;
+                break;
+            case 'model.jsx':
+                fileContent = `export default function use${folderName}() {\n  return {};\n}\n`;
+                break;
+            case 'view.jsx':
+                fileContent = `import React from 'react';\nimport * as Styled from './style';\n\nexport default function ${folderName}View() {\n  return (\n    <Styled.ContainerPage></Styled.ContainerPage>\n  );\n}\n`;
+                break;
+            case 'style.js':
+                fileContent = `import styled from 'styled-components';\n\nexport const ContainerPage = styled.div\`\`;`;
+                break;
+        }
+
+        if (!fs.existsSync(filePath)) {
+            fs.writeFileSync(filePath, fileContent);
+        } else {
+            console.error(`O arquivo ${filePath} já existe. Pulando...`);
+        }
+    });
 
     console.log(`Estrutura de pasta criada com sucesso para "${folderName}"`);
-}).catch(err => console.error(err));
+} catch (err) {
+    console.error(err);
+}
